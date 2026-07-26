28da24907e5573f83900e10ca49d2c75be9b1ab5
Sayyed Faraz Ali <farazalisayyed2728@gmail.com>
2026-07-26 14:18:44 +0530

    add authentication

--- api/views.py
+++ api/views.py
@@ -3,17 +3,24 @@ from rest_framework import viewsets
 from api.models import Company,Employee
 from api.serializers import CompanySerializer,EmployeeSerializer
 from rest_framework.decorators import action
+from rest_framework.response import Response
 
 # Create your views here.
 class CompanyViewSet(viewsets.ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
 
+
+    #companies/{compnayId}/employees
     @action(detail=True, methods=['get'])
     def employees(self,request,pk=None):
-        print("get employees for company id:",pk,"company")
-        pass
-
+        try:
+            company=Company.objects.get(pk=pk)
+            emp=Employee.objects.filter(company=company)
+            emp_serializer = EmployeeSerializer(emp, many=True, context={'request': request})
+            return Response(emp_serializer.data)    
+        except Company.DoesNotExist:
+            return Response({'error': 'Company not found'}, status=404)
 
 
 class EmployeeViewSet(viewsets.ModelViewSet):
