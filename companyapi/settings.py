570e3ce4fd792acadd9038db2579bbbc6980031c
Sayyed Faraz Ali <farazalisayyed2728@gmail.com>
2026-07-26 14:33:54 +0530

    config settings

--- companyapi/settings.py
+++ companyapi/settings.py
@@ -121,5 +121,8 @@ STATIC_URL = 'static/'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+     'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+     ]
 }
